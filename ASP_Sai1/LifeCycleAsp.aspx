3ef892b91bcf6c38f6b82467f0af72ef63c03b0a
SampathNayak1 <megavathsampath1@gmail.com>
2026-04-21 12:39:56 +0530

    Life Cycle Page ASP

---
+++ ASP_Sai1/LifeCycleAsp.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LifeCycleAsp.aspx.cs" Inherits="ASP_Sai1.LifeCycleAsp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+              <asp:Label ID="lblPPreInit" runat="server"></asp:Label><br />
+  <asp:Label ID="lblPInit" runat="server"></asp:Label><br />
+  <asp:Label ID="lblPInitCompelte" runat="server"></asp:Label><br />
+            <asp:Label ID="lblPLoad" runat="server"></asp:Label><br />
+          
+            <asp:Label ID="lblPLoadComplete" runat="server"></asp:Label><br />
+
+            <asp:TextBox ID="txtusername" runat="server" OnTextChanged="txtusername_TextChanged"></asp:TextBox>
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" /><br />
+          
+            <asp:RadioButton ID="rbnTelugu" runat="server" GroupName="language" AutoPostBack="true" OnCheckedChanged="rbnTelugu_CheckedChanged" />Telugu - 
+            <asp:Label ID="lblTelugu" runat="server"></asp:Label> <br />
+            <asp:RadioButton  ID="rbnEnglish" runat="server" GroupName="laguage" AutoPostBack="true" OnCheckedChanged="rbnEnglish_CheckedChanged"/>English - 
+            <asp:Label ID="lblEnglish" runat="server"></asp:Label><br />
+            <asp:RadioButton ID="rbnHindi" runat="server" GroupName="language" AutoPostBack="true" OnCheckedChanged="rbnHindi_CheckedChanged" />Hindi - 
+            <asp:Label ID="lblHindi" runat="server"></asp:Label><br />
+
+            <asp:CheckBox ID="chkChess" AutoPostBack="true" runat="server" OnCheckedChanged="chkChess_CheckedChanged" />Chess
+            <asp:Label ID="lblChess" runat="server"></asp:Label><br />
+
+            <asp:CheckBox ID="chkTennis" AutoPostBack="true" runat="server" OnCheckedChanged="chkTennis_CheckedChanged" />Tennis
+            <asp:Label ID="lblTennis" runat="server"></asp:Label><br /> 
+
+            <asp:CheckBox ID="chkCriket" AutoPostBack="true"  runat="server" OnCheckedChanged="chkCriket_CheckedChanged"/>Cricket
+            <asp:Label ID="lblCricket" runat="server"></asp:Label><br /> 
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP_Sai1/LifeCycleAsp.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_Sai1
+{
+    public partial class LifeCycleAsp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblPLoad.Text = "Hi This is Page_Load ";
+            btnSubmit.Visible = false;
+
+
+        }
+        protected void Page_PreInit(object sender,EventArgs e)
+        {
+            lblPPreInit.Text = "Hi This is Page_PreInit";
+        }
+        protected void Page_Init(object sender,EventArgs e)
+        {
+            lblPInit.Text = "Hi This is Page_Init ";
+        }
+        protected void Page_InitComplete(object sender,EventArgs e)
+        {
+            lblPInitCompelte.Text = "Hi This is Page_PreInit_Complte ";
+        }
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            lblPLoadComplete.Text = "Hi This is Page_LoadComplete";
+        }
+
+        protected void txtusername_TextChanged(object sender, EventArgs e)
+        {
+            if (txtusername.Text.Length > 3)
+            {
+                btnSubmit.Visible = true;   
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void chkTennis_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkChess.Checked)
+            {
+                lblTennis.Text = "Chess is Selected";
+            } 
+        }
+
+        protected void chkChess_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkChess.Checked)
+            {
+                lblChess.Text = "Chess is Selected";
+            }
+        }
+
+        protected void chkCriket_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkCriket.Checked)
+            {
+                lblCricket.Text = "Cricket is selelcted";
+            }
+        }
+
+        protected void rbnTelugu_CheckedChanged(object sender, EventArgs e)
+        { 
+            if (rbnTelugu.Checked)
+            {
+                lblTelugu.Text = "Selelcted Language is Telugu";
+            }
+        }
+
+        protected void rbnEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+               if (rbnEnglish.Checked)
+            {
+                lblEnglish.Text = "Selected Language is English";
+            }
+          
+           
+        }
+
+        protected void rbnHindi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbnHindi.Checked)
+            {
+                lblHindi.Text = "Selected Language is Hinid";
+            }
+        }
+    }
+}
